81b2a37b66cbec58f788288b4f4cf8c37412549f
Karthik000077777 <95902811+Karthik000077777@users.noreply.github.com>
2022-08-08 02:20:32 +0530

    Add files via upload

---
+++ RestAssured/src/test/java/com/ken/RestAssured/DeleteBooking.java
@@ -0,0 +1,54 @@
+package com.ken.RestAssured;
+
+import java.util.Arrays;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class DeleteBooking {
+	static String ss = null;
+	static int statusCode = 0;
+	static String endPoint = "notWorking";
+	static String[][] data = ExcelRead.dataTable;
+	static String statCode = null;
+
+	public static void deleteBooking() {
+		String[] pathParamArray = ReturnPathParam.pathParam("DeleteBooking");
+		String[] contentType = Headers.getHeader("DeleteBooking", "contentType");
+		System.out.println(Arrays.toString(contentType));
+		String[] acceptType = Headers.getHeader("DeleteBooking", "acceptType");
+		System.out.println(Arrays.toString(acceptType));
+		String[] authorization = Headers.getHeader("DeleteBooking", "authorization");
+		System.out.println(Arrays.toString(authorization));
+
+		Response response = 
+				RestAssured.given()
+								.baseUri(GetExcelData.data(data, "DeleteBooking", "BaseURl"))
+								.basePath(GetExcelData.data(data, "DeleteBooking", "BasePath"))
+								.pathParam(pathParamArray[0], pathParamArray[1])
+								.header(authorization[0], authorization[1])
+							.when()
+								.delete();
+				response.then()
+							.statusCode(201)
+							.log()
+							.all();
+
+		String s1 = response.asString();
+
+		statusCode = response.statusCode();
+		statCode = String.valueOf(statusCode);
+
+		String s2 = GetBooking.getBooking(pathParamArray[1]);
+
+		ss = s1 + " " + s2;
+
+		if (ss.contains(s2) || s1.contains("Method Not Allowed")) {
+			endPoint = "Working";
+		} else {
+			endPoint = "notWorking";
+		}
+
+	}
+
+}
---
+++ RestAssured/src/test/java/com/ken/RestAssured/PartialUpdateBooking.java
@@ -0,0 +1,52 @@
+package com.ken.RestAssured;
+
+import java.util.Map;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class PartialUpdateBooking {
+	static String jsonPath = null;
+	static String ss = null;
+	static String jsonPathValidation = null;
+	static String endPoint = "notWorking";
+	static String[][] data = ExcelRead.dataTable;
+	static String statCode = null;
+	static int statusCode = 0;
+
+	public static void partialUpdateBooking() {
+		String[] pathParamArray = ReturnPathParam.pathParam("PartialUpdateBooking");
+		String[] contentType = Headers.getHeader("PartialUpdateBooking", "contentType");
+		String[] acceptType = Headers.getHeader("PartialUpdateBooking", "acceptType");
+		String[] authorization = Headers.getHeader("PartialUpdateBooking", "authorization");
+
+		Response response = 
+				RestAssured.given()
+								.baseUri(GetExcelData.data(data, "PartialUpdateBooking", "BaseURl"))
+								.basePath(GetExcelData.data(data, "PartialUpdateBooking", "BasePath"))
+								.pathParam(pathParamArray[0], pathParamArray[1])
+								.header(authorization[0], authorization[1])
+								.header(contentType[0], contentType[1])
+								.header(acceptType[0], acceptType[1])
+								.body(GetExcelData.data(data, "PartialUpdateBooking", "Req.Body"))
+								.log()
+								.all()
+							.when()
+								.patch();
+					response.then()
+								.log()
+								.all()
+								.statusCode(200);
+
+		Object responseAsObject = response.as(Object.class);
+		Map responseAsMap = (Map) responseAsObject;
+		ss = responseAsMap.entrySet().toString();
+
+		jsonPathValidation = ValidateJsonPath.validateJsonPath(response, "PartialUpdateBooking", "JSONPath");
+		statusCode = response.statusCode();
+		statCode = String.valueOf(statusCode);
+		if (jsonPathValidation.equalsIgnoreCase("pass") && statusCode == 200) {
+			endPoint = "Working";
+		}
+	}
+}
